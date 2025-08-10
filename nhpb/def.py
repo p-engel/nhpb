8bc33916df8cc6a0e8750844170d4f5d10c89506
p-engel <anyanc>
2025-08-10 07:30:47 -0400

    precompute tensor for operator in __init__()

--- nhpb/def.py
+++ nhpb/def.py
@@ -1,47 +1,45 @@
 # define functions
 
 class Operator():
-    par = Parameter();
+    def __init__(self, params):
+        """
+        params: object holding system parameters
+        """
+        self.par = params;
+        # Precompute bases
+        self._basis1 = destroy(self.par.N)
+        self._basis2 = destroy(2)
+        # Precompute operators
+        self._a = tensor(self._basis1, qeye(2))       # mode 1 (cavity)
+        self._sm = tensor(qeye(self.par.N), self._basis2)  # mode 2 (qubit)
 
-    def __init__(self):
-        """Hilbert space"""
-        self.__basis1 = destroy(par.N);
-        self.__basis2 = destroy(2);
-
-    def update_Hilbert(basis1, basis2):
+    def update_Hilbert(self, basis1, basis2):
         self.__basis1 = basis1;
         self.__basis2 = basis2;
+        self._a = tensor(basis1, qeye(2))
+        self._sm = tensor(qeye(self.par.N), basis2)
 
     def mode1(self):
-        """cavity mode operator"""
-        a = tensor(self.__basis1, qeye(2));
-        
-        return a
+        return self._a
 
     def mode2(self):
-        """qubit mode operator"""
-        sm = tensor(qeye(par.N), self.__basis2);
-        
-        return sm
+        return self._sm
 
-    def H(self, w):
-        """
-        JC Hamiltonian in RWA
-        w - drive frequency
-        """
-        Na = self.mode1()*self.mode1().dag();   # number operator
-        Nsm = self.mode2()*self.mode2().dag();
-        X = self.mode1().dag()*self.mode2() + self.mode1()*self.mode2().dag();
-        D = self.mode2() + self.mode2().dag();
+    def JC_H(self, w):
+        """Jaynes-Cummings Hamiltonian in RWA"""
+        Na = self._a * self._a.dag();   # number operator
+        Nsm = self._sm * self._sm.dag();
+        X = self._a.dag()*self._sm + self._a*self._sm.dag();
+        D = self._sm + self._sm.dag();
         
-        H = par.det_wa(w)*Na + par.det_ws(w)*Nsm + par.g*X + par.v*D;
-        
-        return H
+        return (  self.par.det_wa(w) * Na
+                + self.par.det_ws(w) * Nsm 
+                + self.par.g * X 
+                + self.par.v * D  );
 
     def cops(self):
-        """collapse operator"""
+        """collapse operator with n_th = 0"""
         sqrt_kappa = par.kappa ** 0.5;
         sqrt_gamma = par.gamma ** 0.5;
-        cops = [sqrt_kappa*self.mode1(), sqrt_gamma*self.mode2()];   # n_th = 0
-        
-        return cops
+
+        return [sqrt_kappa*self._a, sqrt_gamma*self._sm];
