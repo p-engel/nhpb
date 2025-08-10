4d239634c31e22ddf75316d044bf2258cf884a7d
p-engel <anyanc>
2025-08-10 19:48:54 -0400

    move par variable to namespace module par.py

--- nhpb/def.py
+++ nhpb/def.py
@@ -1,10 +1,11 @@
 # definitions
+import par
 
 class Operator():
-    def __init__(self, pars):
+    def __init__(self, par):
         # Precompute operators
-        self._a = tensor(destroy(pars.N), qeye(2));  # mode 1 (cavity)
-        self._sm = tensor(qeye(pars.N), destroy(2)); # mode 2 (qubit)
+        self._a = tensor(destroy(par.N), qeye(2));  # mode 1 (cavity)
+        self._sm = tensor(qeye(par.N), destroy(2)); # mode 2 (qubit)
 
     def update_operator(self, mode1, mode2):
         self._a = mode1;
@@ -22,27 +23,24 @@ class Operator():
         Nsm = self._sm * self._sm.dag();
         X = self._a.dag()*self._sm + self._a*self._sm.dag();
         D = self._sm + self._sm.dag();
-        return (  pars.det_wa(w) * Na
-                + pars.det_ws(w) * Nsm 
-                + pars.g * X 
-                + pars.v * D  );
+        return (  par.det_wa(w) * Na
+                + par.det_ws(w) * Nsm 
+                + par.g * X 
+                + par.v * D  );
 
     def cops(self):
         """collapse operator with n_th = 0"""
-        sqrt_kappa = pars.kappa ** 0.5;
-        sqrt_gamma = pars.gamma ** 0.5;
+        sqrt_kappa = par.kappa ** 0.5;
+        sqrt_gamma = par.gamma ** 0.5;
         return [sqrt_kappa*self._a, sqrt_gamma*self._sm];
 
-def state(pars):
-    """intial state"""
-    return tensor(basis(pars.N, pars.na), basis(2, pars.nsm));
 
 class Evolve():
     """Evolve density matrix in time"""
     def __init__(self)
-    self._psi0 = state(pars);
-    self._op = Operator(pars);
-    self._dom = Domain(pars);
+    self._psi0 = tensor(basis(par.N, par.na), basis(2, par.nsm));
+    self._op = Operator();
+    self._dom = Domain();
 
     def occupation():
         """occupation number in modes"""
