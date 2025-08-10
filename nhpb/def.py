8daa8d51abc09f65a30d200c9e6cd6241c65f3c8
p-engel <anyanc>
2025-08-10 06:06:56 -0400

    define Operator type

--- nhpb/def.py
+++ nhpb/def.py
@@ -1,1 +1,53 @@
 # define functions
+
+class Operator():
+    par = Parameter();
+
+
+    def __init__(self):
+        """Hilbert space"""
+        self.__basis1 = destroy(par.N);
+        self.__basis2 = destroy(2);
+
+
+    def update_Hilbert(basis1, basis2):
+        self.__basis1 = basis1;
+        self.__basis2 = basis2;
+
+
+    def mode1(self):
+        """cavity mode operator"""
+        a = tensor(self.__basis1, qeye(2));
+        
+        return a
+
+
+    def mode2(self):
+        """qubit mode operator"""
+        sm = tensor(qeye(par.N), self.__basis2);
+        
+        return sm
+
+
+    def H(self, w):
+        """
+        JC Hamiltonian in RWA
+        w - drive frequency
+        """
+        Na = self.mode1()*self.mode1().dag();   # number operator
+        Nsm = self.mode2()*self.mode2().dag();
+        X = self.mode1().dag()*self.mode2() + self.mode1()*self.mode2().dag();
+        D = self.mode2() + self.mode2().dag();
+        
+        H = par.det_wa(w)*Na + par.det_ws(w)*Nsm + par.g*X + par.v*D;
+        
+        return H
+
+
+    def cops(self):
+        """collapse operator"""
+        sqrt_kappa = par.kappa ** 0.5;
+        sqrt_gamma = par.gamma ** 0.5;
+        cops = [sqrt_kappa*self.mode1(), sqrt_gamma*self.mode2()];   # n_th = 0
+        
+        return cops
--- nhpb/h.py
+++ nhpb/h.py
@@ -11,6 +11,7 @@ class Lindbladian():
         """
         self.p = par;  [array, double]
 
+
     def psi0(self)
         """
         intial state
@@ -20,38 +21,5 @@ class Lindbladian():
 
         return psi0
 
-    def operator(self, w)
-        """
-        JC Hamiltonian in RWA
-        w - drive frequency
-        """
-        a = tensor(destroy(self.p.N), qeye(2));
-        sm = tensor(qeye(self.p.N), destroy(2));
-        Na = a.dag()*a;     # number operator for cavity mode
-        Ns = sm.dag()*sm;
-        X = a.dag()*sm + a*sm.dag();    # coupling operator
-        D = sm + sm.dag();  # displacement operator
-        det_wa = self.p.wa - w;     # detuned freq. wrt drive
-        det_ws = self.p.ws - w;
-        g = self.p.g;
-        v = self.p.E;
-        sqrt_kappa = self.p.kappa ** 0.5;
-        sqrt_gamma = self.p.gamma ** 0.5;
-        
-        H = det_wa*Na + det_ws*Ns + g*X + v*D;
-        cops = [sqrt_kappa*a, sqrt_gamma*sm];   # n_th = 0
-
-        return H, cops, a, sm
-
-    def update_par(self, g, det)
-        """
-        modify parameters
-        input - double
-            g - coupling constant betweem modes
-            det - detuning between modes
-        """
-        self.p = Parameter(coupling_const==g, detuning=det);
-
-        return
 
 ####    output = mesolve(H, psi0, tlist, c_ops, [Na, Nsm])
