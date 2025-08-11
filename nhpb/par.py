189527da8a5744b31d01ca156eaed6fb892d0e17
p-engel <anyanc>
2025-08-11 14:53:59 -0400

    create input file as parameter file as par.py

---
+++ nhpb/par.py
@@ -0,0 +1,15 @@
+# System parameters in eV
+# based on measured Fano system in Nano Lett. by Masiello group
+
+# constant
+hbar = 1
+radf_eV = 1.52e15   # [rad/s * 1/eV]
+
+# parameter
+ws = 0.9326
+det = 20e-3
+wc = ws - det
+gamma = 67.5e-3
+kappa = 9.126e-10
+g = 0.332e-3    # coupling constant
+v = 0.2 * 67.5e-3    # drive amplitude
