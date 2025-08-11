7aefc3272cd802b3554029ef4a848b1f96e41aac
p-engel <anyanc>
2025-08-10 20:57:32 -0400

    import declaration in lindbladian namespace; move input to par type

--- nhpb/main.py
+++ nhpb/main.py
@@ -1,20 +1,14 @@
-from h.py import Lindbladian, Domain, evolve
+from lindbladian import Lindbladian 
 
 def main():
     """
-    calculate avergae photon number and second-order correlation
+    calculate avergae occupation number and second-order correlation
     nonHermitian Jaynes-Cummings Hamiltonian
     """
-    # please input coupling constant and detuning parameters
-    g = 0.005*67.5;  # [meV]
-    det = 0;    # [meV]
     try:
-        x = Domain();
-        pars = Parameter(g, det);
-        system = Lindbladian(pars);
-        # system.update_pars(g, det);
-        N, g2 = evolve(x, system);
-        print(N, g2);
+        L = Lindbladian();
+        N = L.occupation()
+        print(N.expect[0], N.expect[1]);
     except (TypeError, ValueError, ZeroDivisionError, AttributeError) as e:
         print(f"Error running calculation: {e}");
     except Exception as e:
