0987af33c8bda93a828f09cc7ff247df04a5bd3f
p-engel <anyanc>
2025-08-14 12:11:05 -0400

    make time domain

--- nhpb/def.py
+++ nhpb/def.py
@@ -1,8 +1,39 @@
 # definitions
+import math
 
 from qutip import tensor, destroy, qeye, mesolve, liouvillian 
 
 import par
+
+class X():
+    """parse par file and create time and frequency grid for sim"""
+    def __init__(self, freq=None):
+        """
+        scale parameters by fastest dissipative rate value
+        freq -- list: float, drive frequency
+        """
+        self.wr = max(par.kappa, par.gamma); # ref freq.
+        self.gamma = par.gamma / self.wr
+        self.kappa = par.kappa / self.wr
+        self.g = par.g / self.wr
+        self.v = par.v / self.wr
+        self.ws = _scaled_detuning(par.ws)
+        self.wc = _scaled_detuning(par.wc)
+
+    def _scaled_detuning(freq0):
+        return [ (freq0 - freq)/self.wr for freq in [par.ws, par.wc] ]
+
+    def times():
+        N = 10; # number of oscillations/decay
+        n = 20; # number of points per oscillations/decay
+        tau_max = max(1/self.g, 1/self.gamma);  # longest life time
+        t_max = N * tau;
+        w_max = max(self.gamma, self.g, self.v, self.ws, self.wc)
+        T_min = 2 * math.pi / w_max;
+        dt = T_min / n;
+        N_step = int(t_max / dt);
+        return [i*dt for i in range(N_steps + 1)]
+        
 
 class Operator():
     def __init__(self):
